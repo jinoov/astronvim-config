098bc740fc89bb9387a0402c608220110a93ce39
jinoov <jinho020@yonsei.ac.kr>
2024-01-27 16:12:18 +0900

    color

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,8 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.html-css" },
--- plugins/user.lua
+++ plugins/user.lua
@@ -35,7 +35,18 @@ return {
     opts = {},
   },
   {
-    "NvChad/nvim-colorizer.lua",
-    opts = {},
+    "norcalli/nvim-colorizer.lua",
+    opts = {
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = true, -- "Name" codes like Blue
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      -- Available modes: foreground, background
+      mode = "foreground", -- Set the display mode.
+    },
   },
 }
